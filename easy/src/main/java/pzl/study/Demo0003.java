c42251cd559e0cd5f56163278959eec0688f13f4
pengzhilin <1272200324@qq.com>
2021-09-15 16:42:56 +0800

    add[21-9-14]:add code 0003

---
+++ easy/src/main/java/pzl/study/Demo0003.java
@@ -0,0 +1,63 @@
+package pzl.study;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+//
+// 示例 1:
+//输入: s = "abcabcbb"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+//
+// 示例 2:
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+//
+// 示例 3:
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+//
+// 示例 4:
+//输入: s = ""
+//输出: 0
+//
+// 提示：
+// 0 <= s.length <= 5 * 104
+// s 由英文字母、数字、符号和空格组成
+//
+// Related Topics 哈希表 字符串 滑动窗口
+// 👍 6113 👎 0
+public class Demo0003 {
+
+    public static void main(String[] args) {
+        Demo0003 demo = new Demo0003();
+        System.out.println(demo.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    /**
+     * 执行耗时:75 ms,击败了11.71% 的Java用户
+     * 内存消耗:39.2 MB,击败了5.77% 的Java用户
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0, count = 0, i = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        while (i < s.length()) {
+            if (map.containsKey(s.charAt(i))) {
+                i = map.get(s.charAt(i)) + 1;
+                map.clear();
+                max = Math.max(max, count);
+                count = 0;
+            }
+            count++;
+            map.put(s.charAt(i), i);
+            i++;
+        }
+        return Math.max(max, count);
+    }
+
+
+}
